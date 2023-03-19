8be3b41f6d2fb0cd18adcc22a7c11bdb4e159eba
PangeranL <117371705+PangeranL@users.noreply.github.com>
2023-03-19 13:17:02 +0700

    Add files via upload

---
+++ NilMax2Tabel.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n; // jumlah elemen dalam tabel
+    printf("Masukkan jumlah elemen dalam tabel: ");
+    scanf("%d", &n);
+
+    int T[n];
+    printf("Masukkan elemen dalam tabel: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &T[i]);
+    }
+
+    int max1 = T[0];
+    int max2 = T[1];
+    if (max1 < max2) {
+        int temp = max1;
+        max1 = max2;
+        max2 = temp;
+    }
+
+    for (int i = 2; i < n; i++) {
+        if (T[i] > max1) {
+            max2 = max1;
+            max1 = T[i];
+        } else if (T[i] > max2 && T[i] != max1) {
+            max2 = T[i];
+        }
+    }
+
+    printf("Nilai maksimum ke-2 adalah %d", max2);
+
+    return 0;
+}
